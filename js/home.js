d8df8a5ac2e403c4c619fa210bc642f498702af2
Kshamina <kshamina.ghelani@carleton.ca>
2018-09-26 12:14:36 -0400

    darken instead of grey home page

--- js/home.js
+++ js/home.js
@@ -1,9 +1,9 @@
 function unGrey(buttonId) {
   var img = buttonId + "-img";
-  $("#" + img).css("filter", "grayscale(0%)");
+  $("#" + img).css("filter", "brightness(100%)");
 }
 
 function grey(buttonId) {
   var img = buttonId + "-img";
-  $("#" + img).css("filter", "grayscale(100%)");
+  $("#" + img).css("filter", "brightness(50%)");
 }
