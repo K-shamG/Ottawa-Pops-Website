149ab085837ce0e81c375bd838b9513c8253ce31
Kshamina <kshamina.ghelani@carleton.ca>
2018-09-29 22:31:35 -0400

    test parallax video

--- js/main.js
+++ js/main.js
@@ -18,12 +18,3 @@ window.onscroll = () => {
     $('#logo').hide();
   }
 };
-
-if(document.location.href.includes("about-us.html")) {
-  var overlay = document.getElementById('mission');
-  var video = document.getElementById('video');
-  video.addEventListener('progress', function() {
-    var show = video.currentTime>=5 && video.currentTime<10;
-    overlay.style.visibility = show? 'visible' : 'visible';
-  }, false);
-}
